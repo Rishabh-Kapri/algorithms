cc8a41cb410c1f0ae39b7bb8413c527bb2c99f1c
Rishabh-Kapri <rishabhkapri@gmail.com>
2022-03-10 00:08:39 +0530

    feat(go): subsets

---
+++ algo_go/subsets/subsets.go
@@ -0,0 +1,13 @@
+package subsets
+
+func Subsets(nums []int)[][] int {
+    subsets := make([][]int, 0)
+    subsets = append(subsets, []int{})
+
+    for _, num := range(nums) {
+        for _, subset := range(subsets) {
+            subsets = append(subsets, append(subset, num))
+        }
+    }
+    return subsets
+}
